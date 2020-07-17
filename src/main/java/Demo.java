3b1fe102655baaa2e73cae66c36a09b233f46456
Aakash Shinghal <aakash.shinghal@connectwise.com>
2020-07-17 10:19:10 +0530

    Modified first file

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,20 +1,30 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Demo {
 
     public static void main(String[] args) {
-        WebDriver driver = initializeFireFoxDriver();
+        WebDriver driver = initalizeDriver("Chrome");
         launchURL(driver);
         getTitle(driver);
 
     }
 
-    public static FirefoxDriver initializeFireFoxDriver() {
-        WebDriverManager.firefoxdriver().setup();
-        return new FirefoxDriver();
+    public static WebDriver initalizeDriver(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("ie") || browser.equalsIgnoreCase("internet explorer")) {
+            WebDriverManager.iedriver().setup();
+            return new InternetExplorerDriver();
+        }
+        return null;
     }
 
     public static void launchURL(WebDriver driver) {
