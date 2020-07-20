1bf5fbaae8217f3068daf38d53f105ea8dd4660c
Aakash Shinghal <aakash.shinghal@connectwise.com>
2020-07-20 09:03:37 +0530

    Added functionality related to Amazon Item selection and moving to cart

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,15 +1,22 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class Demo {
 
     public static void main(String[] args) {
         WebDriver driver = initalizeDriver("Chrome");
-        launchURL(driver);
-        getTitle(driver);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Demo.launchURL(driver);
+        Demo.getTitle(driver);
+        Demo.searchElement(driver);
 
     }
 
@@ -28,11 +35,27 @@ public class Demo {
     }
 
     public static void launchURL(WebDriver driver) {
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
         driver.get("https://www.amazon.com/");
     }
 
     public static void getTitle(WebDriver driver) {
         String title = driver.getTitle();
         System.out.println("Title of the Page = " + title);
+    }
+
+    public static void searchElement(WebDriver driver) {
+        //By customized css selector. Using "Tagname#Id"
+        driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys("a thousand splendid suns");
+        driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys(Keys.ARROW_DOWN);
+        driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("(//div[@class='a-section aok-relative s-image-fixed-height']/img)[1]")).click();
+        driver.findElement(By.cssSelector("#add-to-cart-button")).click();
+        driver.findElement(By.cssSelector("a#hlb-ptc-btn-native")).click();
+        driver.findElement(By.cssSelector("input#ap_email")).sendKeys("abc@gmail.com");
+        driver.findElement(By.cssSelector("#continue")).click();
+        driver.findElement(By.cssSelector("#ap_password")).sendKeys("12345679");
+
     }
 }
