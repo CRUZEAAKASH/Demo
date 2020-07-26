ebcdca10a234a0dac4cba68e6225d023fb391b68
Aakash Shinghal <aakash.shinghal@gmail.com>
2020-07-26 21:21:13 +0530

    Addded window handles methods

---
+++ src/main/java/Pages/MultipleWindows.java
@@ -0,0 +1,18 @@
+package Pages;
+
+import CommomUtil.BaseClass;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
+
+public class MultipleWindows {
+
+    public static void main(String[] args) {
+        BaseClass baseClass = new BaseClass();
+        WebDriver driver = baseClass.initalizeDriver("Chrome");
+        baseClass.launchURL(driver, "https://accounts.google.com/signup");
+        baseClass.getTitle(driver);
+        Set<String> getWindowHandles = driver.getWindowHandles();
+        System.out.println("size of Window handles = " + getWindowHandles.size());
+    }
+}
