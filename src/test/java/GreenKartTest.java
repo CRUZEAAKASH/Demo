be1ac5e806d98af35acd422b2581cf79f6c3bda5
Aakash Shinghal <aakash.shinghal@gmail.com>
2020-07-31 17:22:52 +0530

    Modified GitKart Class Name

--- src/test/java/GreenKartTest.java
+++ src/test/java/GreenKartTest.java
@@ -4,6 +4,11 @@ import org.testng.annotations.Test;
 public class GreenKartTest {
 
     @Test
+    public void greenKartTestToBeExcluded() throws InterruptedException {
+        GreenKart.startGreenKart();
+    }
+
+    @Test
     public void greenKartTest() throws InterruptedException {
         GreenKart.startGreenKart();
     }
