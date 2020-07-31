b8c5b88e40741414acb7642b307558a87b62c28b
Aakash Shinghal <aakash.shinghal@gmail.com>
2020-07-31 20:16:26 +0530

    1. Updated ReadMe.md file 2 Updated Multiple Windows Class

--- src/main/java/Pages/MultipleWindows.java
+++ src/main/java/Pages/MultipleWindows.java
@@ -3,6 +3,7 @@ package Pages;
 import CommomUtil.BaseClass;
 import org.openqa.selenium.WebDriver;
 
+import java.util.Iterator;
 import java.util.Set;
 
 public class MultipleWindows {
@@ -12,8 +13,17 @@ public class MultipleWindows {
         WebDriver driver = baseClass.initializeDriver("firefox");
         baseClass.launchURL(driver, "https://accounts.google.com/signup");
         baseClass.getTitle(driver);
-        Set<String> getWindowHandles = driver.getWindowHandles();
-        System.out.println("size of Window handles = " + getWindowHandles.size());
+        // Storing all the Windows Ids in a set Object
+        Set<String> ids = driver.getWindowHandles();
+        System.out.println("size of Window handles = " + ids.size());
+        //Creating an Iterator Object to iterate over the windows
+        Iterator<String> iterator = ids.iterator();
+        //Storing the id of Parent into ParentId
+        String parentId = iterator.next();
+        //Storing the id of Child into ChildId
+        String childId = iterator.next();
+        //Switching to Child Window
+        driver.switchTo().window(childId);
         baseClass.tearDownDriver(driver);
     }
 }
