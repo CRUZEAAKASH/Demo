0508406e322c8f67ceedc52404fa88881ee563ab
Aakash Shinghal <aakash.shinghal@gmail.com>
2020-07-31 16:26:58 +0530

    1. Added Amazon Test class for TestNG 2. Modified AmazonTest Name

--- src/main/java/Pages/AmazonPages.java
+++ src/main/java/Pages/AmazonPages.java
@@ -5,14 +5,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
-public class Amazon {
+public class AmazonPages {
 
-    public static void main(String[] args) {
+    public static void startAmazon() {
         BaseClass baseClass = new BaseClass();
         WebDriver driver = baseClass.initializeDriver("Chrome");
         baseClass.launchURL(driver, "https://www.amazon.com/");
         baseClass.getTitle(driver);
-        Amazon.amazonOperations(driver);
+        AmazonPages.amazonOperations(driver);
         baseClass.tearDownDriver(driver);
     }
 
@@ -21,8 +21,7 @@ public class Amazon {
         driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys(Keys.ARROW_DOWN);
         driver.findElement(By.cssSelector("#twotabsearchtextbox")).submit();
         driver.findElement(By.xpath("(//div[@class='a-section aok-relative s-image-fixed-height']/img)[1]")).click();
-        driver.findElement(By.cssSelector("#add-to-cart-button")).click();
-        driver.findElement(By.cssSelector("a#hlb-ptc-btn-native")).click();
+        driver.findElement(By.cssSelector("input#checkout-button")).click();
         driver.findElement(By.cssSelector("input#ap_email")).sendKeys("abc@gmail.com");
         driver.findElement(By.cssSelector("#continue")).click();
         driver.findElement(By.cssSelector("#ap_password")).sendKeys("12345679");
