9321e1be75b750013822d65652895a5beca31d07
Aakash Shinghal <aakash.shinghal@gmail.com>
2020-07-31 21:47:46 +0530

    Added Listeners improvement

--- src/test/java/Common/TestNGListeners.java
+++ src/test/java/Common/TestNGListeners.java
@@ -6,58 +6,27 @@ import org.testng.ITestResult;
 
 // ITestListener interface which implements TestNGListeners
 public class TestNGListeners implements ITestListener {
-    /**
-     * Invoked each time before a test will be invoked. The <code>ITestResult</code> is only partially
-     * filled with the references to class, method, start millis and status.
-     *
-     * @param result the partially filled <code>ITestResult</code>
-     * @see ITestResult#STARTED
-     */
+
     @Override
     public void onTestStart(ITestResult result) {
 
     }
 
-    /**
-     * Invoked each time a test succeeds.
-     *
-     * @param result <code>ITestResult</code> containing information about the run test
-     * @see ITestResult#SUCCESS
-     */
     @Override
     public void onTestSuccess(ITestResult result) {
-
+        System.out.println("I succesfully executed Listeners success pass code");
     }
 
-    /**
-     * Invoked each time a test fails.
-     *
-     * @param result <code>ITestResult</code> containing information about the run test
-     * @see ITestResult#FAILURE
-     */
     @Override
     public void onTestFailure(ITestResult result) {
-
+        System.out.println("Failed Test Method = " + result.getName());
     }
 
-    /**
-     * Invoked each time a test is skipped.
-     *
-     * @param result <code>ITestResult</code> containing information about the run test
-     * @see ITestResult#SKIP
-     */
     @Override
     public void onTestSkipped(ITestResult result) {
 
     }
 
-    /**
-     * Invoked each time a method fails but has been annotated with successPercentage and this failure
-     * still keeps it within the success percentage requested.
-     *
-     * @param result <code>ITestResult</code> containing information about the run test
-     * @see ITestResult#SUCCESS_PERCENTAGE_FAILURE
-     */
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 
