61167e7fe00d7fcb750ae28c5a6749ed30fbd38f
Aakash Shinghal <aakash.shinghal@gmail.com>
2020-09-06 18:57:49 +0530

    1. Added QAClickAcademy.feature file which contains 2 scenarios to test QA Click Academy 2. Added QAClickAcademyStepDefinitions.java file structure which will maps all the step definitions file with the method skeleton

---
+++ src/test/java/StepDefinitions/QAClickAcademyStepDefinitions.java
@@ -0,0 +1,42 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class QAClickAcademyStepDefinitions {
+
+    @Given("^User is on Home Page$")
+    public void user_is_on_home_page() {
+
+    }
+
+    @Given("^User in on Home Page$")
+    public void user_in_on_home_page() {
+
+    }
+
+    @Then("^User clicks on Login Button$")
+    public void user_clicks_on_login_button() {
+
+    }
+
+    @Then("^User enters \"([^\"]*)\" as UserName and \"([^\"]*)\" as Password$")
+    public void user_enters_something_as_username_and_something_as_password(String userName, String password) {
+
+    }
+
+    @Then("^User clicks on Forgot Password$")
+    public void user_clicks_on_forgot_password() {
+
+    }
+
+    @Then("^User enters \"([^\"]*)\" as Email$")
+    public void user_enters_something_as_email(String email) {
+
+    }
+
+    @Then("^User clicks on Submit button$")
+    public void user_clicks_on_submit_button() {
+
+    }
+}
