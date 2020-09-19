7a444b63d9e1b888c3fb75dbbd58cc765b1c2891
Aakash Shinghal <aakash.shinghal@gmail.com>
2020-09-19 16:35:20 +0530

    Migrated WebDriverInalization method from this class to runner class

--- src/test/java/StepDefinitions/Amazon/HomePageStepDefinitions.java
+++ src/test/java/StepDefinitions/Amazon/HomePageStepDefinitions.java
@@ -5,13 +5,12 @@ import CommomUtil.WebDriverFactory;
 import Common.DataFromPropertiesFile;
 import PageObjects.Amazon.HomePage;
 import io.cucumber.java.en.Given;
-import org.openqa.selenium.WebDriver;
 
 import java.io.IOException;
 
 public class HomePageStepDefinitions extends BaseClass {
 
-    public WebDriver driver;// = WebDriverFactory.getDriver();
+    //public WebDriver driver;// = WebDriverFactory.getDriver();
     private HomePage homePage;
 
 //    @Before
@@ -25,12 +24,13 @@ public class HomePageStepDefinitions extends BaseClass {
 
     @Given("^User lands on Amazon Home Page$")
     public void user_lands_on_amazon_home_page() throws IOException {
-        this.driver = initializeDriver("chrome");
-        WebDriverFactory.setWebDriver(driver);
+        // this.driver = initializeDriver("chrome");
+        // WebDriverFactory.setWebDriver(driver);
         System.out.println("This is executed");
         String url = DataFromPropertiesFile.getValueFromPropertyFile("url_amazon");
         System.out.println("URL = " + url);
-        launchURL(driver, url);
+        //launchURL(driver, url);
+        launchURL(WebDriverFactory.getDriver(), url);
         System.out.println(url + " is launched");
         homePage = new HomePage();
 
